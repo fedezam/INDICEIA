fe7c4b29bf8409f2ffaea80744b53c68ee980676
fedezam <samprasfed@gmail.com>
2025-10-08 17:14:32 -0300

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,15 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyAiuby8CpYzd1THEXtrx_e0rTDBppX7DdU",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "indiceia.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "indiceia",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "indiceia.appspot.com",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "838723462130",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:838723462130:web:be8268087c6e2ed448c16b",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-YMMMTCT8JR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
