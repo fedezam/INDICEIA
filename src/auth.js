b75e4ca383197fe1a926383f1422379e1f8f8520
fedezam <samprasfed@gmail.com>
2025-10-10 14:03:44 -0300

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -84,7 +84,7 @@ emailLoginForm.addEventListener("submit", async e => {
       const cred = await signInWithEmailAndPassword(auth, email, password);
       user = cred.user;
     }
-    window.location.href = "usuario.html";
+    window.location.href = "/src/pages/usuario.html";
   } catch (error) {
     Utils.showToast(error.message);
   }
@@ -112,7 +112,7 @@ document.getElementById("googleLogin")?.addEventListener("click", async () => {
     }
     
     // Redirigir
-    window.location.href = "usuario.html";
+    window.location.href = "/src/pages/usuario.html";
   } catch(e) { 
     console.error("Error en login Google:", e);
     Utils.showToast("Error al iniciar sesión con Google: " + e.message); 
