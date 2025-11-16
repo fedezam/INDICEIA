c8818507c69bd7374d8c165433948f39964e199c
fedezam <samprasfed@gmail.com>
2025-11-16 18:55:29 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,23 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import fs from 'fs';
 
 export default defineConfig({
   root: '.',
   publicDir: 'public',
-
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        data: {
-          head: fs.readFileSync('./src/components/head.html', 'utf8')
-        }
-      }
-    })
-  ],
-
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        horarios: resolve(__dirname, 'src/pages/horarios.html'),
-        miComercio: resolve(__dirname, 'src/pages/mi-comercio.html'),
-        iaConfig: resolve(__dirname, 'src/pages/ia-config.html'),
-        productos: resolve(__dirname, 'src/pages/productos.html'),
         usuario: resolve(__dirname, 'src/pages/usuario.html'),
+        miComercio: resolve(__dirname, 'src/pages/mi-comercio.html'),
+        horarios: resolve(__dirname, 'src/pages/horarios.html'),
+        productos: resolve(__dirname, 'src/pages/productos.html'),
+        iaConfig: resolve(__dirname, 'src/pages/ia-config.html'),
         dashboard: resolve(__dirname, 'src/pages/dashboard.html'),
       },
     },
   },
-
   server: {
     open: true,
   },
