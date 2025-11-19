d94a4b41025a13b31ad8efc48791a69c2f53a655
fedezam <samprasfed@gmail.com>
2025-11-19 11:19:12 -0300

    Update usuario.jsx

--- src/pages/usuario.jsx
+++ src/pages/usuario.jsx
@@ -1,4 +1,9 @@
 // ==================== IMPORTS ====================
+import '../styles/base.css'
+import '../styles/layout.css'
+import '../styles/components.css'
+import '../styles/forms.css'
+import './usuario.css'
 import {
   doc,
   getDoc,
