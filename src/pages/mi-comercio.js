b264e59a658ed456dc360ee51763090a198963a9
fedezam <samprasfed@gmail.com>
2025-11-24 13:53:17 -0300

    Update mi-comercio.js

--- src/pages/mi-comercio.js
+++ src/pages/mi-comercio.js
@@ -3,7 +3,7 @@ import '../styles/base.css';
 import '../styles/layout.css';
 import '../styles/components.css';
 import '../styles/forms.css';
-import '../styles/orms-premium.css';
+import '../styles/forms-premium.css';
 import './mi-comercio.css';
 import { auth, db } from '../firebase.js';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
