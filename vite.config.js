369df2db27eda6bc20233fb3da5faa018ee5c9a0
fedezam <samprasfed@gmail.com>
2025-12-07 05:58:50 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
         productos: resolve(__dirname, "src/pages/productos.html"),
         "ia-config": resolve(__dirname, "src/pages/ia-config.html"),
         dashboard: resolve(__dirname, "src/pages/dashboard.html"),
+        visual: resolve(__dirname, "src/pages/visual.html"),
       },
     },
   },
