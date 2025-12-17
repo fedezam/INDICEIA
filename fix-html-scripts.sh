00867bda583fac5f20a5569f3129a04252ff7c17
fedezam <samprasfed@gmail.com>
2025-12-17 01:00:28 -0300

    Delete fix-html-scripts.sh

--- fix-html-scripts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "=== Actualizando referencias de .js a .jsx en HTML ==="
-
-for file in src/pages/*.html; do
-  echo "Procesando $file..."
-  sed -i 's/\(\.\/[a-zA-Z0-9_-]*\)\.js/\1.jsx/g' "$file"
-done
-
-echo "✅ Actualización completa."
