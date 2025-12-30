ff675c4bc728a67f34d1f95528a5b95b71af1313
fedezam <samprasfed@gmail.com>
2025-12-30 10:41:41 -0300

    Add new pages to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+
+        // Pages
         usuario: resolve(__dirname, "src/pages/usuario.html"),
         "mi-comercio": resolve(__dirname, "src/pages/mi-comercio.html"),
         horarios: resolve(__dirname, "src/pages/horarios.html"),
@@ -15,9 +17,19 @@ export default defineConfig({
         "ia-config": resolve(__dirname, "src/pages/ia-config.html"),
         dashboard: resolve(__dirname, "src/pages/dashboard.html"),
         visual: resolve(__dirname, "src/pages/visual.html"),
-        stats: resolve(__dirname, "src/pages/stats.html"),           
-        "link-publico": resolve(__dirname, "src/pages/link-publico.html"), 
+        stats: resolve(__dirname, "src/pages/stats.html"),
+        "link-publico": resolve(__dirname, "src/pages/link-publico.html"),
+
+        // Planes
+        Planes: resolve(__dirname, "src/pages/Planes.html"),
+
+        // Pago
+        "pago-exitoso": resolve(
+          __dirname,
+          "src/pages/pago-exitoso.html"
+        ),
       },
     },
   },
 });
+
