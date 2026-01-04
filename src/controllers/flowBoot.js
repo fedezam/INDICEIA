ce267562a0ffd21e87282cf364d3117db7c3ba7a
fedezam <samprasfed@gmail.com>
2026-01-04 18:07:57 -0300

    Delete src/controllers/flowBoot.js

--- src/controllers/flowBoot.js
+++
@@ -1,9 +0,0 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../firebase.js";              // ✅ CORRECTO
-import { runFlowController } from "../flowController.js"; // ✅ CORRECTO
-
-export function bootFlow() {
-  onAuthStateChanged(auth, (user) => {
-    runFlowController(user?.uid);
-  });
-}
