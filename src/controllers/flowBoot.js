7966150e3b47087e687cb22d9e7db9e85435c0c3
fedezam <samprasfed@gmail.com>
2026-01-04 17:30:53 -0300

    Update flowBoot.js

--- src/controllers/flowBoot.js
+++ src/controllers/flowBoot.js
@@ -1,10 +1,11 @@
-// src/boot/flowBoot.js
+// src/controllers/boot/flowBoot.js
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase.js";
-import { runFlowController } from "../controllers/flowController.js";
+import { auth } from "../../firebase.js";              // ✅ sube a src/
+import { runFlowController } from "../flowController.js"; // ✅ mismo nivel
 
 export function bootFlow() {
   onAuthStateChanged(auth, (user) => {
     runFlowController(user?.uid);
   });
 }
+
