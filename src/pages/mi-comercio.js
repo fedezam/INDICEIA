5cd3d0a52cd9231a9eaa4a421ba77866fde6a95b
fedezam <samprasfed@gmail.com>
2026-01-27 08:46:53 -0300

    Update mi-comercio.js

--- src/pages/mi-comercio.js
+++ src/pages/mi-comercio.js
@@ -14,8 +14,6 @@ import './mi-comercio.css';
 import { db } from '../firebase.js';
 import { doc, getDoc, updateDoc, setDoc, Timestamp } from 'firebase/firestore';
 
-// ==================== PLAN MANAGEMENT ====================
-import { applyPlanStateChange } from "../../lib/plan/applyPlanStateChange.js";
 
 // ==================== UTILS ====================
 import { showToast, showLoading, hideLoading } from '../shared/utils.js';
