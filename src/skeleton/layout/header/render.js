02bb50d28dad716b138004d985b8e79853d4f3b5
fedezam <samprasfed@gmail.com>
2026-01-30 17:38:56 -0300

    Update render.js

--- src/skeleton/layout/header/render.js
+++ src/skeleton/layout/header/render.js
@@ -1,34 +1,32 @@
+// src/skeleton/layout/header/render.js
 import './header.css';
 
 export function renderHeader() {
   console.log('🧱 renderHeader()');
 
   const header = document.getElementById('skeleton-header');
+
   if (!header) {
-    console.error('❌ skeleton-header no existe');
+    console.error('❌ #skeleton-header no existe');
     return;
   }
 
   header.innerHTML = `
     <div class="header-left">
-      <div class="logo">🧠</div>
-      <div class="left-text">
-        <div class="brand">INDICEIA</div>
-        <div class="user" id="headerUserName">Usuario</div>
-      </div>
+      <div class="logo">🧠 INDICEIA</div>
+      <div id="headerUserName" class="user-name">Usuario</div>
     </div>
 
     <div class="header-right">
-      <div class="right-text">
-        <div class="commerce" id="headerCommerceName">Mi comercio</div>
-        <div class="plan">
-          <span id="headerPlan">Plan</span>
-          <span id="headerPlanType"></span>
-        </div>
+      <div class="commerce">
+        <div id="headerCommerceName" class="commerce-name">Comercio</div>
+        <div id="headerPlan" class="plan">Plan</div>
       </div>
 
-      <button id="headerLogoutBtn" class="logout">⎋</button>
+      <button id="headerLogoutBtn" class="logout">Salir</button>
     </div>
   `;
+
+  console.log('✅ Header HTML renderizado');
 }
 
