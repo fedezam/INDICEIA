24cf9c562aade60db958a020b7459ddeb4b39cf1
fedezam <samprasfed@gmail.com>
2026-01-30 18:41:59 -0300

    Update render.js

--- src/skeleton/layout/header/render.js
+++ src/skeleton/layout/header/render.js
@@ -11,22 +11,44 @@ export function renderHeader() {
     return;
   }
 
-  header.innerHTML = `
-    <div class="header-left">
-      <div class="logo">🧠 INDICEIA</div>
-      <div id="headerUserName" class="user-name">Usuario</div>
-    </div>
+  // aplicar clase raíz del CSS
+  header.className = 'header';
 
-    <div class="header-right">
-      <div class="commerce">
-        <div id="headerCommerceName" class="commerce-name">Comercio</div>
-        <div id="headerPlan" class="plan">Plan</div>
+  header.innerHTML = `
+    <div class="container">
+
+      <!-- IZQUIERDA -->
+      <div class="logo">
+        <div class="logo-icon">🧠</div>
+        <h1>INDICEIA</h1>
       </div>
 
-      <button id="headerLogoutBtn" class="logout">Salir</button>
+      <!-- DERECHA -->
+      <div class="user-info">
+        <div class="user-details">
+          <div id="headerUserName" class="user-name">
+            Usuario
+          </div>
+
+          <div
+            id="headerPlan"
+            class="plan-badge trial"
+          >
+            Trial
+          </div>
+        </div>
+
+        <button
+          id="headerLogoutBtn"
+          class="btn-logout"
+          title="Cerrar sesión"
+        >
+          ⎋
+        </button>
+      </div>
+
     </div>
   `;
 
-  console.log('✅ Header HTML renderizado');
+  console.log('✅ Header HTML renderizado (layout correcto)');
 }
-
