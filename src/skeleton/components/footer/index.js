ef0239ea68a8841c3baa4e06f3526b31ae4aa5e7
fedezam <samprasfed@gmail.com>
2026-02-03 08:57:31 -0300

    Create index.js

---
+++ src/skeleton/components/footer/index.js
@@ -0,0 +1,5 @@
+// src/skeleton/components/footer/index.js
+// Punto de entrada del componente Footer
+
+export { renderFooter } from './render.js';
+export { updateFooter, initFooter, defaultSocialLinks } from './update.js';
