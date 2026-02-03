318ff2308b9fc3102cbef2dc675fa062921848b6
fedezam <samprasfed@gmail.com>
2026-02-03 08:54:47 -0300

    Create index.js

---
+++ src/skeleton/components/card/index.js
@@ -0,0 +1,5 @@
+// src/skeleton/components/card/index.js
+// Punto de entrada del componente Card
+
+export { renderCard, renderCardWithActions } from './render.js';
+export { updateCard, createAndUpdateCard } from './update.js';
