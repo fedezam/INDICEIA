f0a2a619a450236e4f11d0d9f5d025a0b6024afb
fedezam <samprasfed@gmail.com>
2026-02-05 17:21:44 -0300

    Update index.js

--- src/skeleton/components/card/index.js
+++ src/skeleton/components/card/index.js
@@ -1,7 +1,23 @@
+// /src/skeleton/components/card/index.js
 import { renderCard } from './render.js';
 import { updateCard } from './update.js';
+import './card.css';
 
-export const Card = {
-  render: renderCard,
-  update: updateCard
-};
+/**
+ * Card canónica de Skeleton
+ * @param {Object} data
+ * @param {string} data.title
+ * @param {string|string[]} data.content
+ * @param {string} data.icon - nombre de FontAwesome sin 'fa-'
+ * @param {boolean} [data.highlight=false]
+ * @param {Object} [data.action] - { type: 'link'|'button', ... }
+ * @returns {HTMLElement}
+ */
+export function Card(data) {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = renderCard();
+  const el = wrapper.firstElementChild;
+
+  updateCard(el, data);
+  return el;
+}
