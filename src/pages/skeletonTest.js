99a22e4b513dcc3da1151cb081f754285558de94
fedezam <samprasfed@gmail.com>
2026-02-06 16:12:44 -0300

    Update skeletonTest.js

--- src/pages/skeletonTest.js
+++ src/pages/skeletonTest.js
@@ -4,12 +4,13 @@ import { runSkeleton } from '../skeleton/skeleton.js';
 import { createFirebaseAdapter } from '../skeleton/adapters/firebaseAdapter.js';
 
 import { Card } from '../skeleton/components/card/index.js';
-import { FormField } from '../skeleton/components/formField/index.js';
+import { FormField } from '../skeleton/components/form-field/index.js';
 import { Button } from '../skeleton/components/button/index.js';
 import { showToast } from '../skeleton/components/toast/index.js';
 import { showLoading, hideLoading } from '../skeleton/components/loading/index.js';
 
 import { fillProvinciaSelector } from '../shared/provincias.js';
+
 
 const pageTest = {
   async load(context) {
