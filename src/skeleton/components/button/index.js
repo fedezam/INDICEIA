41bdb533dcb6d31305d87eddb7dd6b1098d83a22
fedezam <samprasfed@gmail.com>
2026-02-07 02:52:34 -0300

    Update index.js

--- src/skeleton/components/button/index.js
+++ src/skeleton/components/button/index.js
@@ -1,31 +1,44 @@
-// index.js
 import { renderButton } from './render';
 import { updateButton } from './update';
 
+/**
+ * Crea un botón interactivo
+ * @param {Object} config
+ * @param {string} config.label - Texto del botón
+ * @param {'primary'|'secondary'|'danger'} [config.variant='primary'] - Estilo visual
+ * @param {Function} [config.onClick] - Callback al hacer click
+ * @param {boolean} [config.disabled=false] - Si está deshabilitado
+ * @param {string} [config.icon] - Nombre del ícono FontAwesome (sin 'fa-')
+ * @returns {HTMLButtonElement & {setLoading: Function, enable: Function, disable: Function, setText: Function}} 
+ * Botón con métodos adicionales:
+ * - setLoading(state: boolean) - Muestra spinner y deshabilita
+ * - enable() - Habilita el botón
+ * - disable() - Deshabilita el botón
+ * - setText(value: string) - Cambia el texto
+ */
 export function createButton(config = {}) {
   const dom = renderButton();
   const btn = updateButton(dom, config);
-
+  
   /* ---------- public API ---------- */
-
   btn.setLoading = (state = true) => {
     btn.classList.toggle('is-loading', state);
     btn.disabled = state;
   };
-
+  
   btn.enable = () => {
     btn.disabled = false;
     btn.classList.remove('is-disabled');
   };
-
+  
   btn.disable = () => {
     btn.disabled = true;
     btn.classList.add('is-disabled');
   };
-
+  
   btn.setText = (value) => {
     dom.text.textContent = value;
   };
-
+  
   return btn;
 }
