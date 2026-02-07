854d513beb09cf6b88ac5dc2c53beeccc84fba11
fedezam <samprasfed@gmail.com>
2026-02-07 02:54:29 -0300

    Update index.js

--- src/skeleton/components/form-field/index.js
+++ src/skeleton/components/form-field/index.js
@@ -1,41 +1,61 @@
-// index.js
 import { renderFormField } from './render';
 import { updateFormField } from './update';
 
+/**
+ * Crea un campo de formulario con label, input y validación
+ * @param {Object} config
+ * @param {string} config.label - Etiqueta del campo
+ * @param {string} config.name - Atributo name del input
+ * @param {'text'|'email'|'password'|'number'|'tel'|'url'|'select'|'textarea'} [config.type='text'] - Tipo de input
+ * @param {string} [config.value=''] - Valor inicial
+ * @param {string} [config.placeholder] - Placeholder del input
+ * @param {boolean} [config.required=false] - Si es obligatorio
+ * @param {boolean} [config.disabled=false] - Si está deshabilitado
+ * @param {Array<{value: string, label: string}>} [config.options] - Opciones (solo para type='select')
+ * @param {number} [config.rows] - Cantidad de filas (solo para type='textarea')
+ * @returns {HTMLElement & {getValue: Function, setValue: Function, setInvalid: Function, isInvalid: Function, disable: Function, enable: Function, getId: Function}}
+ * Elemento con métodos adicionales:
+ * - getValue() - Obtiene el valor actual
+ * - setValue(value) - Establece un nuevo valor
+ * - setInvalid(state: boolean) - Marca como inválido
+ * - isInvalid() - Verifica si está inválido
+ * - disable() - Deshabilita el campo
+ * - enable() - Habilita el campo
+ * - getId() - Obtiene el ID único del campo
+ */
 export function createFormField(config = {}) {
   const dom = renderFormField();
   const input = updateFormField(dom, config);
-
   const el = dom.wrapper;
-
+  
   // ---- API pública ----
   el.getValue = () => input.value;
-
+  
   el.setValue = (value) => {
     input.value = value ?? '';
   };
-
+  
   el.setInvalid = (state) => {
     el.classList.toggle('is-invalid', !!state);
   };
-
+  
   el.isInvalid = () => {
     return el.classList.contains('is-invalid');
   };
-
+  
   el.disable = () => {
     input.disabled = true;
     el.classList.add('is-disabled');
   };
-
+  
   el.enable = () => {
     input.disabled = false;
     el.classList.remove('is-disabled');
   };
-
+  
   el.getId = () => {
     return el.dataset.fieldId;
   };
-
+  
   return el;
 }
