9a320e070c573036da09e8406abe63900dfda9dd
fedezam <samprasfed@gmail.com>
2026-02-10 16:58:50 -0300

    Update index.js

--- src/skeleton/components/onboarding-button/index.js
+++ src/skeleton/components/onboarding-button/index.js
@@ -3,6 +3,14 @@
 import { renderButton } from "./render.js";
 import { attachBehavior } from "./update.js";
 
+/**
+ * Crea un botón de onboarding universal
+ * @param {Object} config
+ * @param {string} config.stepName - Nombre del paso (ej: 'usuario')
+ * @param {Function} config.getData - Función que devuelve los datos a guardar
+ * @param {Function} config.validate - Función que valida si el formulario está completo
+ * @returns {HTMLButtonElement}
+ */
 export function createOnboardingButton(config) {
   const button = renderButton();
   attachBehavior(button, config);
