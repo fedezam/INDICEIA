a635860b39786ad131aa299bdc4b3ece67bf4673
fedezam <samprasfed@gmail.com>
2026-02-12 16:36:12 -0300

    Delete src/skeleton/cognition/cognition.js

--- src/skeleton/cognition/cognition.js
+++
@@ -1,7 +0,0 @@
-import { renderLayout } from "@/skeleton/layout/renderLayout.js";
-import { mountCognition } from "@/skeleton/features/cognition/index.js";
-
-renderLayout();
-
-const main = document.querySelector("main");
-mountCognition(main);
