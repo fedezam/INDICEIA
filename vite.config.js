46d8a33d66094887158b2baa490a5e63681d94b3
fedezam <samprasfed@gmail.com>
2026-02-26 09:53:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
 
         // Pages
         usuario: resolve(__dirname, "src/pages/usuario.html"),
-        "crear-entidad": resolve(__dirname, "src/pages/crear-entidad.html"),
+        "modelo-negocio": resolve(__dirname, "src/pages/modelo-negocio.html"),
         "mi-comercio": resolve(__dirname, "src/pages/mi-comercio.html"),
         horarios: resolve(__dirname, "src/pages/horarios.html"),
         productos: resolve(__dirname, "src/pages/productos.html"),
