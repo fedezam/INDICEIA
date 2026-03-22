07d5ebb22df800c6109ee9c55d47d7bf76ab6b22
fedezam <samprasfed@gmail.com>
2026-03-22 13:33:00 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,23 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-
 export default defineConfig({
   root: ".",
-
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
     },
   },
-
   build: {
     outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-
         // Pages
         usuario: resolve(__dirname, "src/pages/usuario.html"),
         "tipo-entidad": resolve(__dirname, "src/pages/tipo-entidad.html"),
         "mi-comercio": resolve(__dirname, "src/pages/mi-comercio.html"),
         "mi-perfil": resolve(__dirname, "src/pages/mi-perfil.html"),
+        "mi-perfil-profesional": resolve(__dirname, "src/pages/mi-perfil-profesional.html"),
         horarios: resolve(__dirname, "src/pages/horarios.html"),
         productos: resolve(__dirname, "src/pages/productos.html"),
         servicios: resolve(__dirname, "src/pages/servicios.html"),
@@ -31,10 +28,11 @@ export default defineConfig({
         "link-publico": resolve(__dirname, "src/pages/link-publico.html"),
         "capacidades-cognitivas": resolve(__dirname, "src/pages/capacidadesCognitivas.html"),
         entrega: resolve(__dirname, "src/pages/entrega.html"),
-
+        lugares: resolve(__dirname, "src/pages/lugares.html"),
+        cobertura: resolve(__dirname, "src/pages/cobertura.html"),
+        consultas: resolve(__dirname, "src/pages/consultas.html"),
         // Planes
         plans: resolve(__dirname, "src/pages/plans.html"),
-
         // Pago
         "pago-exitoso": resolve(__dirname, "src/pages/pago-exitoso.html"),
       },
