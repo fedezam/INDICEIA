7927a2559aa05b30bf13996548a441964165660c
fedezam <samprasfed@gmail.com>
2026-04-15 18:21:22 +0000

    localidades ok

--- src/shared/geo-helpers.js
+++ src/shared/geo-helpers.js
@@ -1,7 +1,7 @@
 // src/shared/geo-helpers.js
 
-import vecinos from './vecinos.json';
-import arGeo from './ar-geo-enriched.json';
+import vecinos from './vecinos.json'        with { type: 'json' };
+import arGeo   from './ar-geo-enriched.json' with { type: 'json' };
 
 function toPath(str) {
   return str
