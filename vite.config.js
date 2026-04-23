9acf42862da952f037616bd773aca1373cb519e8
fedezam <samprasfed@gmail.com>
2026-04-23 09:19:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
         lugares: resolve(__dirname, "src/pages/lugares.html"),
         cobertura: resolve(__dirname, "src/pages/cobertura.html"),
         consultas: resolve(__dirname, "src/pages/consultas.html"),
+        "super-admin": resolve(__dirname, "src/pages/super-admin.html"),
         // Planes
         plans: resolve(__dirname, "src/pages/plans.html"),
         // Pago
