f30efb3fc75207f5dd6a4e7bc899146b5636553a
fedezam <samprasfed@gmail.com>
2026-05-10 09:40:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,58 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+
 export default defineConfig({
   root: ".",
+
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
     },
   },
+
   build: {
     outDir: "dist",
+
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+
         // Pages
         usuario: resolve(__dirname, "src/pages/usuario.html"),
         "tipo-entidad": resolve(__dirname, "src/pages/tipo-entidad.html"),
+
         "mi-comercio": resolve(__dirname, "src/pages/mi-comercio.html"),
         "mi-perfil": resolve(__dirname, "src/pages/mi-perfil.html"),
         "mi-perfil-profesional": resolve(__dirname, "src/pages/mi-perfil-profesional.html"),
+
         horarios: resolve(__dirname, "src/pages/horarios.html"),
+
         productos: resolve(__dirname, "src/pages/productos.html"),
         servicios: resolve(__dirname, "src/pages/servicios.html"),
+
         "ia-config": resolve(__dirname, "src/pages/ia-config.html"),
+
         dashboard: resolve(__dirname, "src/pages/dashboard.html"),
         visual: resolve(__dirname, "src/pages/visual.html"),
         stats: resolve(__dirname, "src/pages/stats.html"),
+
         "link-publico": resolve(__dirname, "src/pages/link-publico.html"),
         "capacidades-cognitivas": resolve(__dirname, "src/pages/capacidadesCognitivas.html"),
+
         entrega: resolve(__dirname, "src/pages/entrega.html"),
         lugares: resolve(__dirname, "src/pages/lugares.html"),
         cobertura: resolve(__dirname, "src/pages/cobertura.html"),
         consultas: resolve(__dirname, "src/pages/consultas.html"),
+
         "admin-login": resolve(__dirname, "src/pages/admin-login.html"),
-        //"_sys-panel": resolve(__dirname, "src/pages/_sys-panel.html"),
+
         // Planes
         plans: resolve(__dirname, "src/pages/plans.html"),
+
         // Pago
         "pago-exitoso": resolve(__dirname, "src/pages/pago-exitoso.html"),
-        // 🆕 PANEL
+
+        // Super Admin
         "super-admin": resolve(__dirname, "src/pages/super-admin.html"),
       },
     },
