56d67d8a23da984613acdb366c37bfe0840bde2a
fedezam <samprasfed@gmail.com>
2026-05-12 09:16:19 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -33,6 +33,7 @@ export default defineConfig({
         "ia-config": resolve(__dirname, "src/pages/ia-config.html"),
         "llm-parser-test": resolve(__dirname, "src/pages/llm-parser-test.html"),
         "ler-browser-test": resolve(__dirname, "src/pages/ler-browser-test.html"),
+        "entidad-pizeria-la-esquina": resolve(__dirname, "src/pages/carga/y/habita/entidad-pizeria-la-esquina.html"),
 
         dashboard: resolve(__dirname, "src/pages/dashboard.html"),
         visual: resolve(__dirname, "src/pages/visual.html"),
