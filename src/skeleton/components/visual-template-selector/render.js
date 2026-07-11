2b0b60ec0398e2cf09510cd5e4ce49afe35d3792
fedezam <samprasfed@gmail.com>
2026-07-11 09:06:03 -0300

    Create render.js

---
+++ src/skeleton/components/visual-template-selector/render.js
@@ -0,0 +1,9 @@
+// skeleton/components/visual-template-selector/render.js
+import './styles.css';
+
+export function renderTemplateSelector() {
+  const grid = document.createElement('div');
+  grid.className = 's-visual-grid';
+
+  return { grid };
+}
